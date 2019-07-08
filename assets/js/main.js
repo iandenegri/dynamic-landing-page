48268d1b0f75ebb53de4e108fe33ad3f8561679a
Ian Denegri <iandenegri@hotmail.com>
2019-07-08 18:34:26 -0400

    Adding local storage support. Finishing project

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ const focus = document.getElementById("focus");
 
 // Show current time and update every second.
 
-function show_time() {
+function showTime() {
     let today = new Date();
     let hour = today.getHours();
     let minutes = today.getMinutes();
@@ -26,13 +26,95 @@ function show_time() {
     };
 
     // Output our time into the time html element
-    time.innerHTML = `${hour}<span>:</span>${minutes}<span>:</span>${seconds}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(minutes)}<span>:</span>${addZero(seconds)} ${AMPM}`;
 
     // Call the new time every second
-    setTimeout(show_time, 1000);
+    setTimeout(showTime, 1000);
 };
 
 // Fix time format. Minutes and seconds don't show a leading zero.
+function addZero(n){
+    if(parseInt(n, 10) < 10){
+        return ("0" + n);
+    } else {
+        return n;
+    };
+};
+
+// Set background image and greeting based on the time of day.
+function setGreeting(){
+    let today = new Date();
+    let hour = today.getHours();
+
+    if(hour < 12 && hour > 5){
+        greeting.textContent = "Good Morning, ";
+        document.body.style.backgroundImage = "url('../assets/imgs/morning.jpg')";
+    } else if(hour < 18 && hour > 12){
+        greeting.textContent = "Good Afternoon, ";
+        document.body.style.backgroundImage = "url('../assets/imgs/afternoon.jpg')";
+    } else {
+        greeting.textContent = "Good Evening, ";
+        document.body.style.backgroundImage = "url('../assets/imgs/night.jpg')";
+        document.body.style.color = "white";
+    };
+};
+
+// Get user's name
+function getName(){
+    if(localStorage.getItem('name') === null){
+        name.textContent = '{{ name }}';
+    } else {
+        name.textContent =  localStorage.getItem('name');
+    }
+};
+
+// Set user's name
+function setName(event){
+    if(event.type === 'keypress'){
+        // Validate that the keypress is enter
+        if (event.which == 13 || event.keycode == 13){
+            localStorage.setItem('name', event.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', event.target.innerText);
+    }
+}
+
+// Get user's focus
+function getFocus(){
+    if(localStorage.getItem('focus') === null){
+        focus.textContent = '{{ focus }}';
+    } else {
+        focus.textContent =  localStorage.getItem('focus');
+    }
+};
+
+// Set user's focus
+function setFocus(event){
+    if(event.type === 'keypress'){
+        // Validate that the keypress is enter
+        if (event.which == 13 || event.keycode == 13){
+            localStorage.setItem('focus', event.target.innerText);
+            focus.blur();
+        }
+    } else {
+        localStorage.setItem('focus', event.target.innerText);
+    }
+}
+
+// Add listeners for updating preferences.
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 
 // Run the script!
-show_time();
+showTime();
+
+// Set the greeting message and background
+setGreeting();
+
+// Set up the storage of the user's name and focuslocally.
+getName();
+getFocus();
